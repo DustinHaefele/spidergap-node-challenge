2582a1a2534ae4389b52f72528c07125d4b8fe32
Dustin Haefele <Dustin.haefele@gmail.com>
2020-01-28 16:32:45 -0500

    Calculating distance between two points

--- src/customerInvite.js
+++ src/customerInvite.js
@@ -1,10 +1,26 @@
 const fs = require('fs');
 
 
+let data = fs.readFileSync('./partners.json')
+let partners = JSON.parse(data);
 
 
-fs.readFile('./partners.json', (err, data) => {
-  if (err) throw err;
-  let partners = JSON.parse(data);
-  console.log(partners);
-});
+function findDistance(coordString) {
+
+  const londLat = 51.515419 * Math.PI / 180;
+  const londLong = -0.141099 * Math.PI / 180;
+  let coorArray = coordString.split(',').map(coor => parseFloat(coor));
+  const custLat = coorArray[0] * Math.PI / 180;
+  const custLong = coorArray[1] * Math.PI / 180;
+  const centerAngle = (Math.acos((Math.sin(custLat)*Math.sin(londLat))+(Math.cos(custLat)*Math.cos(londLat)*Math.cos(Math.abs(custLong - londLong)))));
+
+  return centerAngle * 6371;
+
+}
+
+console.log(findDistance("-33.8934219,151.20404600000006"));
+
+
+
+
+
