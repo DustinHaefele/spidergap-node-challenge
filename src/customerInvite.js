4ee8a3dbab86b29389275bd602fd3108d853c2c8
Dustin Haefele <Dustin.haefele@gmail.com>
2020-01-28 17:48:28 -0500

    updated variable names

--- src/customerInvite.js
+++ src/customerInvite.js
@@ -7,31 +7,31 @@ function findDistance(coordString) {
   const londLat = (51.515419 * Math.PI) / 180;
   const londLong = (-0.141099 * Math.PI) / 180;
   let coorArray = coordString.split(',').map(coor => parseFloat(coor));
-  const custLat = (coorArray[0] * Math.PI) / 180;
-  const custLong = (coorArray[1] * Math.PI) / 180;
+  const partnerLat = (coorArray[0] * Math.PI) / 180;
+  const partnerLong = (coorArray[1] * Math.PI) / 180;
   const centerAngle = Math.acos(
-    Math.sin(custLat) * Math.sin(londLat) +
-      Math.cos(custLat) *
+    Math.sin(partnerLat) * Math.sin(londLat) +
+      Math.cos(partnerLat) *
         Math.cos(londLat) *
-        Math.cos(Math.abs(custLong - londLong))
+        Math.cos(Math.abs(partnerLong - londLong))
   );
 
   return centerAngle * 6371;
 }
 
-function findCustomers(custArray) {
-  let invitedCust = [];
-  custArray.forEach(cust => {
-    for (let i = 0; i < cust.offices.length; i++) {
-      if (findDistance(cust.offices[i].coordinates) <= 100) {
-        invitedCust.push({
-          companyName: cust.organization,
-          address: cust.offices[i].address
+function findPartners(partnerArray) {
+  let invitedPartner = [];
+  partnerArray.forEach(partner => {
+    for (let i = 0; i < partner.offices.length; i++) {
+      if (findDistance(partner.offices[i].coordinates) <= 100) {
+        invitedPartner.push({
+          companyName: partner.organization,
+          address: partner.offices[i].address
         });
       }
     }
   });
-  invitedCust.sort((a, b) => {
+  invitedPartner.sort((a, b) => {
     if (a.companyName < b.companyName) {
       return -1;
     } else {
@@ -39,7 +39,7 @@ function findCustomers(custArray) {
     }
   });
 
-  return invitedCust;
+  return invitedPartner;
 }
 
-console.log(findCustomers(partners));
+console.log(findPartners(partners));
