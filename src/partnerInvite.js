b6ab4a5ec2c37563505f45a5d3a5779b9b89fcce
Dustin Haefele <Dustin.haefele@gmail.com>
2020-01-28 19:26:14 -0500

    added comments and removed an unneeded log

--- src/partnerInvite.js
+++ src/partnerInvite.js
@@ -3,29 +3,41 @@ const fs = require('fs');
 let data = fs.readFileSync('./partners.json');
 let partners = JSON.parse(data);
 
+//Function to find the the distance between coordinates given as a string and the center of london.
+//Coordinate pairs are formatted as a comma seperated string in the partner data.
 function findDistance(coordString) {
+
+  //Converting london coordinates to radians
   const londonLat = (51.515419 * Math.PI) / 180;
   const londonLong = (-0.141099 * Math.PI) / 180;
 
+  //Extracting partner coordinates from the string and converting to radians
   let coordArray = coordString.split(',').map(coor => parseFloat(coor));
   const partnerLat = (coordArray[0] * Math.PI) / 180;
   const partnerLong = (coordArray[1] * Math.PI) / 180;
 
+  //Using the formula to find the center angle
   const centerAngle = Math.acos(
     Math.sin(partnerLat) * Math.sin(londonLat) +
       Math.cos(partnerLat) *
         Math.cos(londonLat) *
         Math.cos(Math.abs(partnerLong - londonLong))
   );
-
+  
+  //Earths mean radius of 6371 times the center angle gives you the distance between the coordinate pairs
   return centerAngle * 6371;
 }
 
+//function to find the partners within 100km
 function findPartners(partnerArray) {
   let invitedPartner = [];
+
+  //looking at each individual partner
   partnerArray.forEach(partner => {
+    //looking at all the offices for a partner and seeing if any are within 100km.
     for (let i = 0; i < partner.offices.length; i++) {
       if (findDistance(partner.offices[i].coordinates) <= 100) {
+        //pushing just the data we need to the array that holds the partners
         invitedPartner.push({
           companyName: partner.organization,
           address: partner.offices[i].address
@@ -33,6 +45,7 @@ function findPartners(partnerArray) {
       }
     }
   });
+  //sorting the matching partners in ascending order
   invitedPartner.sort((a, b) => {
     if (a.companyName < b.companyName) {
       return -1;
@@ -46,7 +59,6 @@ function findPartners(partnerArray) {
   return invitedPartner;
 }
 
-console.log(findPartners(partners));
 
 module.exports = {
   findPartners,
